98d6a13986a5625bbd043ecd5f29468844733c84
tgiqfe <tqji31001@gmail.com>
2024-05-27 18:09:22 +0900

    update copied message.

--- GazoView/Lib/InfoPanel.xaml.cs
+++ GazoView/Lib/InfoPanel.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using GazoView.Lib.Conf;
+using MaterialDesignThemes.Wpf;
+using System.Printing;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace GazoView.Lib
 {
@@ -12,6 +16,58 @@ namespace GazoView.Lib
         {
             InitializeComponent();
             this.DataContext = Item.BindingParam;
+        }
+
+        private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ViewCopiedMessage();
+        }
+
+        private void ViewCopiedMessage()
+        {
+            PackIcon packIcon = new()
+            {
+                Kind = PackIconKind.TextBoxCheck,
+                Width = 24,
+                Height = 24,
+                Foreground = Brushes.White,
+                Padding = new Thickness(0),
+                HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(2, 2, 2, 2),
+            };
+
+            Label label = new()
+            {
+                Content = "Copied!",
+                FontSize = 14,
+                FontFamily = new FontFamily("Noto Sans JP"),
+                Foreground = Brushes.White,
+                HorizontalContentAlignment = HorizontalAlignment.Left,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(0, 3, 0, 3),
+                Padding = new Thickness(0),
+            };
+
+            StackPanel stackPanel = new()
+            {
+                Orientation = Orientation.Horizontal,
+                Margin = new Thickness(20, 150, 0, 0),
+                Width = 100,
+                Height = 28,
+                Background = Brushes.DimGray,
+            };
+            stackPanel.Children.Add(packIcon);
+            stackPanel.Children.Add(label);
+
+            InfoPanelGrid.Children.Add(stackPanel);
+            Task.Delay(3000).ContinueWith(_ =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    InfoPanelGrid.Children.Remove(stackPanel);
+                });
+            });
         }
     }
 }
