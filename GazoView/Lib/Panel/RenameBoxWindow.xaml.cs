6f3063ddb39bcf0979783fb4b1bb8a99755c5e8c
tgiqfe <tqji31001@gmail.com>
2026-04-16 17:34:12 +0900

    Update Rename methods.

--- GazoView/Lib/Panel/RenameBoxWindow.xaml.cs
+++ GazoView/Lib/Panel/RenameBoxWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.IO;
 using MaterialDesignThemes.Wpf;
+using System.Diagnostics;
 
 namespace GazoView.Lib.Panel
 {
@@ -57,6 +58,20 @@ namespace GazoView.Lib.Panel
                 case Key.Enter:
                     RenameImageFile();
                     break;
+                case Key.Up:
+                    if (TextBoxForFileName.IsFocused)
+                    {
+                        UpDownNumber(1);
+                        e.Handled = true;
+                    }
+                    break;
+                case Key.Down:
+                    if (TextBoxForFileName.IsFocused)
+                    {
+                        UpDownNumber(-1);
+                        e.Handled = true;
+                    }
+                    break;
             }
         }
 
@@ -71,7 +86,7 @@ namespace GazoView.Lib.Panel
             {
                 Item.BindingParam.Images.RenameImageFile($"{TextBoxForFileName.Text}{TextBlockForExtension.Text}");
                 Item.BindingParam.RenameBox.HideWindow();
-            }   
+            }
         }
 
         /// <summary>
@@ -107,5 +122,69 @@ namespace GazoView.Lib.Panel
                 _renamable = true;
             }
         }
+
+        #region up,down key for number selection in file name
+
+        /// <summary>
+        /// Updown number in textbox.
+        /// </summary>
+        /// <param name="offset"></param>
+        private void UpDownNumber(int offset)
+        {
+            if (TextBoxForFileName.SelectionLength == 0)
+            {
+                var caretIndex = TextBoxForFileName.CaretIndex;
+                var text = TextBoxForFileName.Text;
+                var numberString = GetNumberStringAtPosition(text, caretIndex);
+
+                if (!string.IsNullOrEmpty(numberString))
+                {
+                    var startIndex = text.IndexOf(numberString, Math.Max(0, caretIndex - numberString.Length));
+                    if (startIndex >= 0)
+                    {
+                        TextBoxForFileName.Select(startIndex, numberString.Length);
+                    }
+                }
+            }
+            string selectedText = TextBoxForFileName.SelectedText;
+            int newNumber = int.Parse(selectedText) + offset;
+            string newSelectedText = newNumber.ToString().PadLeft(selectedText.Length, '0');
+            string newText = TextBoxForFileName.Text.
+                Remove(TextBoxForFileName.SelectionStart, TextBoxForFileName.SelectionLength).
+                Insert(TextBoxForFileName.SelectionStart, newSelectedText);
+            int newSelectionStart = TextBoxForFileName.SelectionStart;
+            TextBoxForFileName.Text = newText;
+            TextBoxForFileName.Select(newSelectionStart, selectedText.Length);
+        }
+
+        /// <summary>
+        /// Get number string at the specified position in the text.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        private string GetNumberStringAtPosition(string text, int position)
+        {
+            if (string.IsNullOrEmpty(text) || position < 0 || position > text.Length) return string.Empty;
+
+            int currentPos = position == text.Length ? position - 1 : position;
+            if (currentPos < 0 || currentPos >= text.Length || !char.IsDigit(text[currentPos])) return string.Empty;
+
+            int startPos = currentPos;
+            while (startPos > 0 && char.IsDigit(text[startPos - 1]))
+            {
+                startPos--;
+            }
+
+            int endPos = currentPos;
+            while (endPos < text.Length - 1 && char.IsDigit(text[endPos + 1]))
+            {
+                endPos++;
+            }
+
+            return text.Substring(startPos, endPos - startPos + 1);
+        }
+
+        #endregion
     }
 }
